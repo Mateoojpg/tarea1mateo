0d7cd1967da7da5f6e0d9ae8fe0993cff099f809
Mateo <mateo.gutierrez@manquemavida.cl>
2023-06-14 09:54:26 -0400

    añadi la lista de numeros primos

--- funciones.py
+++ funciones.py
@@ -1,4 +1,3 @@
-
 def es_primo(numero):
     if numero < 2:
         return False
@@ -6,3 +5,10 @@ def es_primo(numero):
         if numero % i == 0:
             return False
     return True
+
+def lista_primos_menores(numero):
+    primos_menores = []
+    for i in range(2, numero):
+        if es_primo(i):
+            primos_menores.append(i)
+    return primos_menores
