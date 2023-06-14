79a3b9b4239d9ea099bb99907aee9c58cfa3d019
Mateo <mateo.gutierrez@manquemavida.cl>
2023-06-14 09:52:14 -0400

    codigo para saber si un numero es primo o no

--- funciones.py
+++ funciones.py
@@ -1,1 +1,8 @@
-print("hola pussy")
+
+def es_primo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, int(numero**0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
